7155246eee58dc0a36972e81b3d956953894ac23
Yahya Hussein <yahyahussein91@gmail.com>
2015-09-23 18:22:13 -0700

    final code for rps game

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,19 +22,16 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    var playerMove = move || getInput();
-    return playerMove;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
-    var computerMove = move || randomPlay();
-    return computerMove;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
 
-       var winner;
 if (PlayerMove === ComputerMove) {
    winner = "Tie";
 }
@@ -64,13 +61,34 @@ else (PlayerMove === "Paper") {
 }
    return winner;
 }
+getWinner();
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    var playerMove;
+    var computerMove;
+    var winner;
+
+    while (playerWins<=5 || computerWins<=5){
+        playerMove = getPlayerMove();
+        computerMove = getComputerMove();
+        winner = getWinner(playerMove,computerMove);
+        if (winner === "player"){
+            playerWins +=1;
+        }
+        else if (winner === "computer"){
+            computerWins +=1;
+        }
+        else{
+            computerWins += 0;
+            playerWins+= 0;
+        }
+        console.log('Player chose ' + playerMove + " while Computer chose " + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }
     return [playerWins, computerWins];
 }
 
+playToFive();
